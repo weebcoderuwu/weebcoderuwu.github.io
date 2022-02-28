65b9f4f6cfd83779bb1395d905c8f9d4dd6a8e4c
weebcoderuwu <adam3752YT@gmail.com>
2022-02-28 15:28:31 +0100

    lol

--- ASStolfo/osustats.js
+++ ASStolfo/osustats.js
@@ -1,5 +1,15 @@
-axios.get('https://osutracker.com/api/users/14534655').then(res => {
-    console.log(res.data)
-})
- 
+const API_URL = "https://osutracker.com/api/"
 
+const xhr = new XMLHttpRequest();
+
+function OnRequestHandeler() {
+   if (this.redystate == 4 && this.status == 200)
+   
+   console.log(this.response);
+}
+
+
+xhr.addEventListener("load",OnRequestHandeler);
+xhr.open("GET" , `${API_URL}/users/{14534655}`);
+// mi user id es 14534655
+xhr.send();
