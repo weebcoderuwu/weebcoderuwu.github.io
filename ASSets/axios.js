893bd72ba1b0ff72dbfcfca51acd274e2d082df8
weebcoderuwu <adam3752YT@gmail.com>
2022-02-28 22:25:15 +0100

    dftbgv6hydtgvsrgfc5tv

--- ASSets/axios.js
+++ ASSets/axios.js
@@ -1,8 +1,10 @@
 const OsuStats = async() => {
-  const statsdata = await axios.get("https://osutracker.com/api/users/14534655/stats")
-    
-  console.log(statsdata);
-
-}
-
-OsuStats();
+    const statsdata = await axios.get("https://osutracker.com/api/users/14534655/stats")
+      
+    console.log(statsdata.data.results[0]);
+  
+  }
+  
+  OsuStats();
+  
+  
