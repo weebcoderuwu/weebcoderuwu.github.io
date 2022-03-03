96487eab0d7b4e8320e047c66ee6211f19f487a6
weebcoderuwu <adam3752YT@gmail.com>
2022-03-04 00:56:03 +0100

    a
    a

--- ASSets/axios.js
+++ ASSets/axios.js
@@ -1,7 +1,7 @@
 const OsuStats = async() => {
     const statsdata = await axios.get("https://osutracker.com/api/users/14534655/stats")
       
-    console.log(statsdata.data.results[0]);
+    console.log(statsdata.data.results);
   
   }
   
