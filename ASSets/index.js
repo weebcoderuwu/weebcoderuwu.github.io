0243088ed45a2bef5485e02eff18ae5adf206843
weebcoderuwu <adam3752YT@gmail.com>
2022-03-16 18:03:59 +0100

    fdv
    xcvcbcvb

---
+++ ASSets/index.js
@@ -0,0 +1,11 @@
+let xhr = new XMLHttpRequest();
+xhr.open('get', 'https://osutracker.com/api/users/14534655');
+xhr.send();
+
+xhr.onload = function() {
+    console.log(xhr.response);
+
+    asd =(xhr.response)
+};
+
+HTMLParamElement 
