73520794919147f6f54cd05f61e7028b7acb542e
weebcoderuwu <adam3752YT@gmail.com>
2022-03-30 20:09:55 +0200

    finaly,  osu! stats

--- ASStolfo/index.js
+++ ASStolfo/index.js
@@ -1,30 +1,23 @@
+//api call
+
 let xhr = new XMLHttpRequest();
 xhr.open('get', 'https://osutracker.com/api/users/14534655');
 xhr.send();
-
+// fun fact: i forgot to put the script tag in the html file 
+// and i spent a lot of time trying to figure out why it wouldn't work
 xhr.onload = function() {
 
     console.log(xhr.response);
 
-    
 
-
+    //convert to JSON
     let data = JSON.parse(xhr.response);
-    let name = data.name;
-    let recent_scores = data.recent_scores;
-    let recent_plays = data.recent_plays;
-    let recent_events = data.recent_events;
-    let recent_achievements = data.recent_achievements;
-    let recent_beatmaps = data.recent_beatmaps;
-    let recent_plays_count = data.recent_plays_count;
-    let recent_scores_count = data.recent_scores_count;
-    let recent_achievements_count = data.recent_achievements_count;
-    let recent_beatmaps_count = data.recent_beatmaps_count;
-    let pp = data.pp;
 
-    
-
+    //display on html
     document.getElementById("preformance-points").innerHTML = data.pp;
-
+    document.getElementById("rank").innerHTML = data.rank;
+    document.getElementById("level").innerHTML = data.level;
+    document.getElementById("acc").innerHTML = data.acc;
+    document.getElementById("top").innerHTML = data.currentTop;
 };
 
